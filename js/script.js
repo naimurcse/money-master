60172b73b987be0937cfefbae60a03491ab4479f
naimurcse <2015naim@gmail.com>
2022-05-18 20:33:43 +0600

    button to input-type-button fix issue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("Hello Bangladesh");
+// document.getElementById("income-input");
+// document.getElementById("food-expense-input");
+// document.getElementById("rent-expense-input");
+// document.getElementById("clothes-expense-input");
+
+document.getElementById("calculate-btn").addEventListener("click", function() {
+    console.log("I'm Clicked");
+});
