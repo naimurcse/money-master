f0a6836932173f8f01da348215b2842c9e507d02
naimurcse <2015naim@gmail.com>
2022-05-18 21:26:00 +0600

    Get All the input fields value and convert them float number

--- js/script.js
+++ js/script.js
@@ -1,8 +1,46 @@
-// document.getElementById("income-input");
-// document.getElementById("food-expense-input");
-// document.getElementById("rent-expense-input");
-// document.getElementById("clothes-expense-input");
+// const incomeInput = document.getElementById("income-input");
+
+// let foodExpenseInput = document.getElementById("food-expense-input");
+// const rentExpenseInput = document.getElementById("rent-expense-input");
+// const clothesExpenseInput = document.getElementById("clothes-expense-input");
+
+
+
+const displayTotalExpenses = document.getElementById("display-total-expenses");
+const displayBalance = document.getElementById("display-balance");
+
+const displaySavingAmount = document.getElementById("display-saving-amount");
+const displayRemainingBalance = document.getElementById("display-remaining-balance");
+
+
+function getValue(id) {
+    let inputValue = document.getElementById(id + "-input");
+    inputValue = parseFloat(inputValue.value);
+    return inputValue;
+}
+
+function getDisplayValue(id) {
+    let displayValue = document.getElementById("display-" + id);
+    displayValue = parseFloat(displayValue.innerText);
+    return displayValue;
+}
 
 document.getElementById("calculate-btn").addEventListener("click", function() {
-    console.log("I'm Clicked");
+
+    // Get Value From Income Input Field
+    let incomeInput = getValue("income");
+    console.log(incomeInput, typeof incomeInput);
+
+    // Get Value From Food Expense Input Field
+    let foodExpenseInput = getValue("food-expense");
+    console.log(foodExpenseInput, typeof foodExpenseInput);
+
+    // Get Value From Rent Expense Input Field
+    let rentExpenseInput = getValue("rent-expense");
+    console.log(rentExpenseInput, typeof rentExpenseInput);
+
+    // Get Value From Clothes Expense Input Field
+    let clothesExpenseInput = getValue("clothes-expense");
+    console.log(clothesExpenseInput, typeof clothesExpenseInput);
+
 });
