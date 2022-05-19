9d95cdded1de0d033ba09dc58fc2be93ad6c3b47
naimurcse <2015naim@gmail.com>
2022-05-19 13:54:53 +0600

    Income Area Error Handle Done

--- js/script.js
+++ js/script.js
@@ -7,8 +7,20 @@ const displayRemainingBalance = document.getElementById("display-remaining-balan
 // Define getValue() Function
 function getValue(id) {
     let inputValue = document.getElementById(id + "-input");
-    inputValue = parseFloat(inputValue.value);
-    return inputValue;
+    inputValue = inputValue.value;
+    if (isNaN(inputValue)) {
+        // console.log("The amount allows only numbers.");
+        return "NaN-inputed"
+    } else if (inputValue == "") {
+        // console.log("Please Enter an amount");
+        return "empty-inputed"
+    } else if (inputValue < 0) {
+        // console.log("Please Enter Positive Amount");
+        return "negative-value-inputed"
+    } else {
+        inputValue = parseFloat(inputValue);
+        return inputValue;
+    }
 }
 
 // Define getDisplayValue() Function
@@ -23,31 +35,75 @@ function getTotalExpenses() {
     return getValue("food-expense") + getValue("rent-expense") + getValue("clothes-expense");
 }
 
+// Get Error Massage
+function getErrorMassage(err, id, field) {
+    let displayError = document.getElementById(id);
+    console.log(displayError);
+    if (err == "NaN-inputed") {
+        displayError.innerHTML = `
+            Give number amount of <span class="fw-bold">${field} </span>
+        `;
+    } else if (err == "empty-inputed") {
+        displayError.innerHTML = `
+            Give an amount of <span class="fw-bold">${field}</span>
+        `;
+    } else if (err == "negative-value-inputed") {
+        displayError.innerHTML = `
+            Give an positive amount of <span class="fw-bold">${field} </span>
+        `;
+    } else {
+        displayError.innerHTML = "";
+    }
+}
 
+function isError(incomeInput) {
+    if (incomeInput == "NaN-inputed" || incomeInput == "empty-inputed" || incomeInput == "negative-value-inputed") {
+        return true;
+    } else {
+        return false;
+    }
+}
 // Event handle by Calculate Btn
 document.getElementById("calculate-btn").addEventListener("click", function() {
+    let incomeErrorArea = document.getElementById("income-error-area");
+    let expenseErrorArea = document.getElementById("experses-error-area");
 
     // Get Value From Income Input Field
     let incomeInput = getValue("income");
-    console.log(incomeInput, typeof incomeInput);
+    // console.log(incomeInput, typeof incomeInput);
 
-    // Get Value From Food Expense Input Field
-    let foodExpenseInput = getValue("food-expense");
-    console.log(foodExpenseInput, typeof foodExpenseInput);
+    if (isError(incomeInput)) {
+        getErrorMassage(incomeInput, "income-err", "Income");
+    } else {
+        incomeErrorArea.innerHTML = "";
+        let foodExpenseInput = getValue("food-expense");
+        // console.log(foodExpenseInput, typeof foodExpenseInput);
 
-    // Get Value From Rent Expense Input Field
-    let rentExpenseInput = getValue("rent-expense");
-    console.log(rentExpenseInput, typeof rentExpenseInput);
+        // Get Value From Rent Expense Input Field
+        let rentExpenseInput = getValue("rent-expense");
+        // console.log(rentExpenseInput, typeof rentExpenseInput);
 
-    // Get Value From Clothes Expense Input Field
-    let clothesExpenseInput = getValue("clothes-expense");
-    console.log(clothesExpenseInput, typeof clothesExpenseInput);
+        // Get Value From Clothes Expense Input Field
+        let clothesExpenseInput = getValue("clothes-expense");
+        // console.log(clothesExpenseInput, typeof clothesExpenseInput);
 
-    // const totalExpenses = foodExpenseInput + rentExpenseInput + clothesExpenseInput;
-    displayTotalExpenses.innerText = getTotalExpenses();
-
-    let totalBalance = incomeInput - getTotalExpenses();
-    displayBalance.innerText = totalBalance;
+        if (isError(foodExpenseInput)) {
+            getErrorMassage(foodExpenseInput, "expense-err", "Food Expense");
+            console.log(foodExpenseInput, "ERROR FOOD");
+        } else if (rentExpenseInput == "NaN-inputed" || rentExpenseInput == "empty-inputed" || rentExpenseInput == "negative-value-inputed") {
+            getErrorMassage(rentExpenseInput, "expense-err", "Rent Expense");
+            console.log(rentExpenseInput, "ERROR FOOD");
+        } else if (clothesExpenseInput == "NaN-inputed" || clothesExpenseInput == "empty-inputed" || clothesExpenseInput == "negative-value-inputed") {
+            getErrorMassage(clothesExpenseInput, "expense-err", "Clothes Expense");
+            console.log(clothesExpenseInput, "ERROR FOOD");
+        } else {
+            // const totalExpenses = foodExpenseInput + rentExpenseInput + clothesExpenseInput;
+            displayTotalExpenses.innerText = getTotalExpenses();
+            let totalBalance = incomeInput - getTotalExpenses();
+            displayBalance.innerText = totalBalance;
+            expenseErrorArea.innerHTML = "";
+        }
+    }
 });
 
 // Event handle by Save Btn
